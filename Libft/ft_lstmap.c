f9d9af2f41b34945ea9acf25ebae6da668aa9795
Margherita Mafalda Dallolio <mdalloli@c1r5p4.42firenze.it>
2024-11-28 14:56:59 +0100

    corretto

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -36,3 +36,42 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return (new_list);
 }
+/*
+void	*to_uppercase(void *content)
+{
+	char *str = (char *)content;
+	char *new_str = malloc(ft_strlen(str) + 1);
+	size_t	i = 0;
+	if (!new_str)
+		return (NULL);
+	while (str[i])
+	{
+		new_str[i] = (str[i] >= 'a' && str[i] <= 'z') ? str[i] - 32 : str[i];
+		i++;
+	}
+	new_str[ft_strlen(str)] = '\0';
+	return (new_str);
+}
+
+void	del_content(void *content)
+{
+	free(content);
+}
+
+int	main(void)
+{
+	t_list	*lst = NULL;
+	ft_lstadd_back(&lst, ft_lstnew(ft_strdup("spero")));
+	ft_lstadd_back(&lst, ft_lstnew(ft_strdup("sia")));
+	ft_lstadd_back(&lst, ft_lstnew(ft_strdup("giusto")));
+	printf("Lista originale:\n");
+	for (t_list *temp = lst; temp; temp = temp->next)
+        	printf("%s\n", (char *)temp->content);
+	t_list *mapped_list = ft_lstmap(lst, to_uppercase, del_content);
+	printf("\nLista mappata:\n");
+	for (t_list *temp = mapped_list; temp; temp = temp->next)
+  		printf("%s\n", (char *)temp->content);
+	ft_lstclear(&lst, del_content);
+	ft_lstclear(&mapped_list, del_content);
+	return (0);
+}*/
