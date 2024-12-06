c6b1c23a1607cfca382a76fb08c20b8212a7bd64
Margherita Mafalda Dallolio <mdalloli@c1r5p4.42firenze.it>
2024-12-06 11:49:04 +0100

    libft

---
+++ Mileston 0/Libft/ft_putnbr_fd.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdalloli <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/22 11:49:26 by mdalloli          #+#    #+#             */
+/*   Updated: 2024/11/22 11:49:27 by mdalloli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static void	print_digits(int number, int fd)
+{
+	if (number == 0)
+	{
+		return ;
+	}
+	print_digits(number / 10, fd);
+	ft_putchar_fd((number % 10) + '0', fd);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		print_digits(147483648, fd);
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = -n;
+	}
+	if (n == 0)
+		ft_putchar_fd('0', fd);
+	else
+		print_digits(n, fd);
+}
---
+++ Mileston 0/Libft/ft_substr.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdalloli <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/22 10:13:15 by mdalloli          #+#    #+#             */
+/*   Updated: 2024/11/22 10:13:18 by mdalloli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*dest;
+	size_t	i;
+	size_t	slen;
+	size_t	sublen;
+
+	if (!s)
+		return (NULL);
+	slen = ft_strlen(s);
+	if (start >= slen)
+		return (ft_strdup(""));
+	sublen = slen - start;
+	if (sublen > len)
+		sublen = len;
+	dest = (char *)malloc(sizeof(char) * (sublen + 1));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (i < sublen)
+	{
+		dest[i] = s[start + i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+int main(void)
+{
+	char *string = "Hello, world!";
+	char *result;
+
+	result = ft_substr(string, 7, 5);
+	if (result)
+	{
+		printf("Originale: %s, Inizio: %u, Lunghezza: %zu, 
+				Risultato: %s\n", string, 7, 5, result);
+		free(result);
+	}
+	return (0);
+}*/
