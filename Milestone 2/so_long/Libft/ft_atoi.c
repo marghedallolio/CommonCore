efecc942abae961fd42b1f658b34969a05d2875f
Margherita Mafalda Dallolio <mdalloli@c1r5p4.42firenze.it>
2025-01-21 11:32:27 +0100

    inizio so_long

---
+++ Milestone 2/so_long/Libft/ft_atoi.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdalloli <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/21 10:30:22 by mdalloli          #+#    #+#             */
+/*   Updated: 2024/11/21 10:30:24 by mdalloli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	result;
+	int	i;
+	int	sign;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result = result * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+/*
+int	main(void)
+{
+	printf("my atoi :%d\n", ft_atoi("42"));
+	printf("og atoi :%d\n", atoi("42"));
+}*/
